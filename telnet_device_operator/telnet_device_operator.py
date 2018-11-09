fd64b07bb7571a5f4252efd31ee11816aa4d8961
Aleksandr Ivanov <alexander.ivanov.35@gmail.com>
2018-11-09 14:07:15 +0900

    fail connection will be catched

--- telnet_device_operator/telnet_device_operator.py
+++ telnet_device_operator/telnet_device_operator.py
@@ -1,56 +1,68 @@
 import telnetlib
 import json
+from socket import gaierror
 
 class IOSTelnetOperator():
-	def __init__(self, host, username, password, port='23'):
-		self.hostname = host
-		self.username = username
-		self.password = password
-		self.port = port
+    def __init__(self, host, username, password, port='23'):
+        self.hostname = host
+        self.username = username
+        self.password = password
+        self.port = port
 
-	def connect(self):
-		self.connection = telnetlib.Telnet(self.hostname)
-		self.connection.read_until(b"Username: ")
-		self.connection.write(bytes((self.username+'\n').encode('ascii')))
-		self.connection.read_until(b"Password: ")
-		self.connection.write(bytes((self.password+'\n').encode('ascii')))
-		self.device_name = self.connection.read_until(b"#")
-		self.device_name = bytes(self.device_name.decode('ascii').split('\r\n')[-1], 'ascii')
-		self.connection.write(bytes(('terminal length 0'+'\n').encode('ascii')))
-		self.connection.read_until(self.device_name)
+    def connect(self):
+        try:
+            self.connection = telnetlib.Telnet(self.hostname)
+            self.connection.read_until(b"Username: ")
+            self.connection.write(bytes((self.username+'\n').encode('ascii')))
+            self.connection.read_until(b"Password: ")
+            self.connection.write(bytes((self.password+'\n').encode('ascii')))
+            self.device_name = self.connection.read_until(b"#")
+            self.device_name = bytes(self.device_name.decode('ascii').split('\r\n')[-1], 'ascii')
+            self.connection.write(bytes(('terminal length 0'+'\n').encode('ascii')))
+            self.connection.read_until(self.device_name)
+        except gaierror as exc:
+            return {'hostname_error': exc}
+        except TimeoutError as exc:
+            return {'timeout_error': exc}
+        except EOFError as exc:
+            return {'eof_error': exc}
+        except Exception as exc:
+            return {'other_exception': exc}
 
-	def close(self):
-		self.connection.close()
+    def close(self):
+        self.connection.close()
 
-	# def config_operation(self, operation, proc_func):
-	# 	self.connection.write(bytes((operation+'\n\n').encode('ascii')))
-	# 	# output = self.connection.read_until(self.device_name+bytes('(config)','ascii'))
-	# 	output = self.connection.read_some()
-	# 	output = self.connection.read_some()
-	# 	return proc_func(output)	
+    # def config_operation(self, operation, proc_func):
+    #   self.connection.write(bytes((operation+'\n\n').encode('ascii')))
+    #   # output = self.connection.read_until(self.device_name+bytes('(config)','ascii'))
+    #   output = self.connection.read_some()
+    #   output = self.connection.read_some()
+    #   return proc_func(output)
 
-	# show etc ...
-	def show_operation(self, operation):
-		self.connection.write(bytes((operation+'\n').encode('ascii')))
-		output = self.connection.read_until(self.device_name)	
-		return output	
+    # show etc ...
+    def show_operation(self, operation):
+        self.connection.write(bytes((operation+'\n').encode('ascii')))
+        output = self.connection.read_until(self.device_name)
+        return output
 
-	def get_device_name(self):
-		return self.device_name			 	
-
+    def get_device_name(self):
+        return self.device_name
 
 
 def test_func(raw_data):
-	print(raw_data)
+    print(raw_data)
 
 def main():
-	ITO = IOSTelnetOperator('ipv4', 'username', 'password')
-	ITO.connect()
-	output = ITO.show_operation('show running-config')
-	# output = ITH.config_operation('config', test_func)
+    ITO = IOSTelnetOperator('ipv4', 'username', 'password')
+    status = ITO.connect()
+    if status is not None:
+        print(status)
+        exit(1)
+    output = ITO.show_operation('show running-config')
+    # output = ITH.config_operation('config', test_func)
 
-	print(output)
-	print(ITO.get_device_name())
+    print(output)
+    print(ITO.get_device_name())
 
 if __name__ == '__main__':
-	main()
+    main()
